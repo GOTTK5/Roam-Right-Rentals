59833bc9ebb5b27017f7619cad2b34ff92cd703c
GOTTK5 <gottkruppk@gmail.com>
2026-02-06 12:24:26 +0530

    created routes for Create operation and show individual listing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');  // acquire path module for handling file paths fo
 
 app.set('view engine', 'ejs');  // set ejs as view engine
 app.set('views', path.join(__dirname, 'Views')); // set views directory to Views folder in the current directory
+app.use(express.urlencoded({ extended: true })); // middleware to parse urlencoded data from forms, extended true allows for rich objects and arrays to be encoded into the urlencoded format
 
 main()  // connect to database
    .then( ()=> {  // if connection is successful
@@ -20,15 +21,31 @@ async function main( ){  // async function to connect to database as mongoose co
     await mongoose.connect('mongodb://127.0.0.1:27017/majorproject');   // connect to mongodb database named majorproject
 };
 
-
+ // index route
 app.get("/listings", async ( req, res)=> {  // route to get all listings from database, async as fetching from database takes time
         const listings = await Listing.find({});  // fetch all listings from database using listing model, await as it takes time to fetch from database  
         res.render("listings/index.ejs", { listings });  // render listings.ejs view and pass listings data to it
 });
 
+// New Route
+app.get("/listings/new", ( req, res) => {  // route to render form for creating new listing
+    res.render("listings/new.ejs");  // render new.ejs view
+});
+
+// Create Route
+app.post("/listings", async ( req, res) => {  // route to create new listing in database, async as saving to database takes time
+    const newListing = new Listing(req.body);  // create new listing object from form data in request body
+    await newListing.save();  // save new listing to database using listing model, await as it takes time to save to database
+    res.redirect("/listings");  // redirect to listings index page after creating new listing
+});
 
 
-
+ // show route for individual listing
+app.get("/listing/:id", async ( req, res) => {  // route to get individual listing by id from database, async as fetching from database takes time
+    const { id } = req.params;  // get id from request parameters
+    const listing = await Listing.findById(id);  // fetch listing by id from database using listing model, await as it takes time to fetch from database
+    res.render("listings/show.ejs", { listing });  // render show.ejs view and pass listing data to it
+});
 
 
 
